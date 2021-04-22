494396e6762930b18e4033980bfe572e09576663
ryutaro-kodama <slj-flz-m2@ezweb.ne.jp>
2021-04-22 11:58:21 +0900

    Change version.

--- app/src/main/java/jp/ac/titech/itpro/sdl/helloandroid/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/helloandroid/MainActivity.java
@@ -1,6 +1,7 @@
 package jp.ac.titech.itpro.sdl.helloandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
